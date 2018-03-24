2d29fad31fd56b33be493b0a6d9957919d197165
lwei2 <luweidreamlife@foxmail.com>
2018-03-24 23:44:31 +0800

    add master

---
+++ C/Examples/HelloWorld.c
@@ -0,0 +1,14 @@
+/*************************************************************************
+    > File Name: HelloWorld.c
+    > Author: lwei
+    > Mail: root@163.com 
+    > Created Time: 2018年03月24日 星期六 23时36分03秒
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main(int argc,char *argv[])
+{
+	printf("Hello World!");
+	return 0;
+}
