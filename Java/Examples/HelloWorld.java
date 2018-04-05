7544e64e3a7ce08f6a865f62671072e1b0db8aff
lwei2 <luweidreamlife@foxmail.com>
2018-04-05 11:47:08 +0800

    add master

---
+++ Java/Examples/HelloWorld.java
@@ -0,0 +1,12 @@
+/*************************************************************************
+    > File Name: HelloWorld.java
+    > Author: lwei
+    > Mail: root@163.com 
+    > Created Time: 2018年03月24日 星期六 23时36分03秒
+ ************************************************************************/
+public class HelloWorld{
+	/* This is first Java process */
+	public static void main(String []args){
+		System.out.println("hello world!");
+	}
+}
---
+++ Java/README.md
@@ -0,0 +1,7 @@
+#Java
+1.ubuntu安装jdk
+	a.下载jdk
+	b.解压
+	c.环境配置
+	d.检查是否已安装成功java -version
+
